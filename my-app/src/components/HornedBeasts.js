3bf025eb653bd754548e62e1f6eec728ab135dda
Baraah Alomari <balomari995@gmail.com>
2021-05-17 16:45:33 +0300

    finish lab 2

--- my-app/src/components/HornedBeasts.js
+++ my-app/src/components/HornedBeasts.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+
 class HornedBeasts extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            numberOfPets:0,
+        }
+    }
+
+    increseNumber = ()=>{
+        this.setState({
+            numberOfPets: this.state.numberOfPets + 1,
+        })
+    }
+
+
 render(){
     return(
         <div>
-            <h2>{this.props.title}</h2>
-            <img src={this.props.imgURL}/>
+            {/* <h2>{this.props.title}</h2>
+            <img onClick={this.increseNumber} src={this.props.imgURL}/>
             <p>{this.props.description}</p>
+            <p>{this.state.numberOfPets}</p> */}
+
+        <Card style={{ width: '18rem' }}>
+        <Card.Img onClick={this.increseNumber} variant="top" src={this.props.imgURL}/>
+        <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>
+            🐏 Numper of animals: {this.state.numberOfPets}
+            </Card.Text>
+            
+        </Card.Body>
+        </Card>
+
+
         </div>    
     )
 }
