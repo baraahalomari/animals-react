5d629f3345af04b76d1b3c35432a972327f99a01
Baraah Alomari <balomari995@gmail.com>
2021-05-18 19:01:05 +0300

    adding model

--- my-app/src/components/HornedBeasts.js
+++ my-app/src/components/HornedBeasts.js
@@ -6,14 +6,17 @@ class HornedBeasts extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            numberOfPets:0,
-        }
+            numberOfAnimals:0,
+        };
     }
 
     increseNumber = ()=>{
         this.setState({
-            numberOfPets: this.state.numberOfPets + 1,
+            numberOfAnimals: this.state.numberOfAnimals + 1,
         })
+    }
+    sendInfo =()=>{
+        this.props.handleShow(this.props.title);
     }
 
 
@@ -23,14 +26,14 @@ render(){
             {/* <h2>{this.props.title}</h2>
             <img onClick={this.increseNumber} src={this.props.imgURL}/>
             <p>{this.props.description}</p>
-            <p>{this.state.numberOfPets}</p> */}
+            <p>{this.state.numberOfAnimals}</p> */}
 
         <Card style={{ width: '18rem' }}>
-        <Card.Img onClick={this.increseNumber} variant="top" src={this.props.imgURL}/>
+        <Card.Img onClick={this.sendInfo} variant="top" src={this.props.imgURL}/>
         <Card.Body>
             <Card.Title>{this.props.title}</Card.Title>
             <Card.Text>
-            🐏 Numper of animals: {this.state.numberOfPets}
+            🐏 Numper of animals: {this.state.numberOfAnimals}
             </Card.Text>
             <Card.Text>
              {this.props.description}
